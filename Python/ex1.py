371e0be3f5da21dca60163538354fb7a56bd5895
Alex <alex@alexcoke.com>
2013-05-18 12:59:39 -0700

    updated exersice

---
+++ Python/ex1.py
@@ -0,0 +1,7 @@
+print "hello World!"
+print "hello again"
+print "i like typing thing"
+print "this is fun"
+print "Yay! Printing"
+print "i'd much rather you 'not'."
+print 'i "said" do not touch this'
---
+++ Python/ex2.py
@@ -0,0 +1,9 @@
+# a comment, this is so you can read your program later.
+# Anythig after the # is ignored by python.
+
+print "I could hav code like this." # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a pice of code:
+# print "this wont run"
+
+print "this will run"
